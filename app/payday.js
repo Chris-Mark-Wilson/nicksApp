bcf5b4cd901f3ce3710d8142402f6d328ec75aee
Chris-Mark-Wilson <chris@cmwilson.co.uk>
2024-09-22 18:43:25 +0100

    add wage calculation to payday

--- app/payday.js
+++ app/payday.js
@@ -12,6 +12,8 @@ const navigation = useNavigation();
 const [date,setDate]=useState(new Date());
 const [workers,setWorkers]=useState([]);
 const [selectedWorker,setSelectedWorker]=useState(null);  
+const [jobs,setJobs]=useState([]);
+const [paydays,setPaydays]=useState([]);
 const [paid,setPaid]=useState(0); 
 const [owed,setOwed]=useState(0);
 
@@ -25,6 +27,20 @@ useEffect(()=>{
         console.log('error reading value');
       }
     }
+    const getJobs = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('jobs');
+        return jsonValue != null ? JSON.parse(jsonValue) : [];
+      } catch(e) {
+        console.log('error reading value');
+      }
+    }
+    if(!jobs.length){
+      getJobs()
+      .then((data)=>{
+        setJobs(data);
+      }
+    )}
 
   if(workers.length===0){
     getWorkers()
@@ -37,36 +53,62 @@ useEffect(()=>{
 
   useEffect(()=>{
     // work out the total paid and total owed for the selected worker
-    if(selectedWorker){
-    const [totalPaid,totalOwed]=calculateWages(selectedWorker);
+    if(selectedWorker && jobs.length>0){
+      console.log('calculating wages',selectedWorker,jobs);
+    const [totalPaid,totalOwed]=calculateWages(selectedWorker,jobs);
     setPaid(totalPaid);
     setOwed(totalOwed);
     }
-  },[selectedWorker]);
+  },[selectedWorker,jobs]);
 
-  const calculateWages = (worker) => {
-    const totalWorked=getTotalWorked(worker);//total of cash amount worker has worked based on his rate FOR THAT JOB!
+  const calculateWages = (worker,jobs) => {
+    const totalWorked=getTotalWorked(worker,jobs);//total of cash amount worker has worked based on his rate FOR THAT JOB!
     const totalPaid=getTotalPaid(worker);//total of cash amount worker has been paid from all payments ever made to them
+    console.log('paid',totalPaid);
+    console.log('worked',totalWorked);  
     return [totalPaid,totalWorked];
  
   }
   
-  const getTotalWorked=(worker)=>{
-    const total=0;
+  const getTotalWorked=(worker,jobs)=>{
+    let total=0;
     //iterate through all jobs,iterate through days worked
     //find the selected worker if exists in each day
     //if exists, add worker.rate to total
     //return total
+jobs.forEach((job)=>{
+  if(!job.dates_worked){
+    return;
+  }
+  console.log('Dates_worked',job.dates_worked);
+  job.dates_worked.forEach((day)=>{
+    console.log('Day',day);
+    console.log('Worker',worker);
+    day.workers.forEach((w)=>{
+    if(w.name===worker.name){
+      total+=worker.rate;
+    }
+  });
+});
+});
   return total;
   }
 
   const getTotalPaid=(worker)=>{
-    const total=0;
-    //need a new paydays object in storage
+    let total=0;
+    //need a new paydays object in workers.worker.paydays, array of paydays, payday = {date: date, amount: amount}
     //if paydays exists
     //iterate through all paydays
-    //find the selected worker IF EXISTS in each payday
+    //if payday.date is before or equal to today
     //add worker.paid to total
+    //return total
+    if(worker.paydays){
+      worker.paydays.forEach((payday)=>{
+        if(payday.date<=date){
+          total+=payday.amount;
+        }
+      });
+    }
     return total;
   }
 
